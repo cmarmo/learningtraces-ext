57adfedae8f5bd06635084103d7184edc8261502
Chiara Marmo <chiara.marmo@universite-paris-saclay.fr>
2024-10-17 16:28:43 +0200

    Settings are in place

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { INotebookTracker, NotebookActions } from '@jupyterlab/notebook';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { NotebookActions } from '@jupyterlab/notebook';
 import { CodeCellModel } from '@jupyterlab/cells'
+
+const PLUGIN_ID = 'learning-traces-extension:plugin';
 
 async function writelt(content: String) {
   try {
@@ -20,11 +23,47 @@ async function writelt(content: String) {
  * Initialization data for the jupyterlab learning traces extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'learning-traces-extension:plugin',
+  id: PLUGIN_ID,
   description: 'A jupyter extension that save learning traces when executing a notebook.',
   autoStart: true,
-  requires: [INotebookTracker],
-  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
+  requires: [ ISettingRegistry],
+  activate: (
+    app: JupyterFrontEnd,
+    settings: ISettingRegistry
+  ) => {
+    console.log('JupyterLab extension learning_traces_extension is activated!');
+
+    let learningtag = "";
+
+    /**
+     * Load the settings for this extension
+     *
+     * @param settings Extension settings
+     */
+    function loadSetting(setting: ISettingRegistry.ISettings): void {
+      // Read the settings and convert to the correct type
+      learningtag = setting.get('learningtag').composite as string;
+      console.log(
+        `Learning Traces Extension Settings: learningtag is set to '${learningtag}'`
+      );
+    }
+
+    // Wait for the application to be restored and
+    // for the settings for this plugin to be loaded
+    Promise.all([app.restored, settings.load(PLUGIN_ID)])
+      .then(([, setting]) => {
+        // Read the settings
+        loadSetting(setting);
+
+        // Listen for your plugin setting changes using Signal
+        setting.changed.connect(loadSetting);
+      })
+      .catch(reason => {
+        console.error(
+          `Something went wrong when reading the settings.\n${reason}`
+        );
+      });
+
     NotebookActions.executed.connect((_, args) => {
       const { cell, notebook, success } = args;
       if (cell.model.type === 'code') {
